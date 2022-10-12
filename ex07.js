6a280646d5e4dd64e57423a67270b09ec8e4258b
marcelonunes2 <mklelo1990@gmail.com>
2022-10-12 14:30:32 -0300

    exercicios

--- ex07.js
+++ ex07.js
@@ -1,8 +1,8 @@
 // [ ] Faça um programa onde leia um numero e diga se ele é par ou impar.
 
-const numero = 5
+const numero = 1
 
-if (numero / 2){
+if (numero % 2 === 0){
     console.log("O numero",numero, "é Par")
 }else{
     console.log("O numero",numero, "é impar")
